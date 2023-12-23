8d9e4bdd15b1e88fc98f37794e1d58bb9182995e
girish12upadhye <girishupadhye12@gmailcom>
2023-12-23 21:42:24 +0530

    conflict resolve

--- src/main/java/com/UGit/app/App.java
+++ src/main/java/com/UGit/app/App.java
@@ -4,6 +4,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	String str = "Girish";
 	System.out.println("Welcome to UGit this is girish tech universe");
     }
 }
