a286730e9f13d1ba528b20b597fed1ee0efb7d7f
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2023-12-23 21:37:12 +0530

    Update App.java

--- src/main/java/com/UGit/app/App.java
+++ src/main/java/com/UGit/app/App.java
@@ -4,6 +4,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	    int x = 10;
 	System.out.println("Welcome to UGit this is girish tech universe");
     }
 }
