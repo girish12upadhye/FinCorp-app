ce44555b950723fdf04f7b503c815d4c8582e982
girish12upadhye <girishupadhye12@gmailcom>
2023-12-23 21:34:35 +0530

    this girish tech universe

--- src/main/java/com/UGit/app/App.java
+++ src/main/java/com/UGit/app/App.java
@@ -4,6 +4,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println("Welcome to UGit");
+	System.out.println("Welcome to UGit this is girish tech universe");
     }
 }
